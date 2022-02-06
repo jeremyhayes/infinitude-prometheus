458503e313cbd1d0e53a771356b319aca0f161c5
Jeremy Hayes <jeremyhayes@users.noreply.github.com>
2022-02-06 01:25:56 +0000

    disable default collectors

--- main.go
+++ main.go
@@ -4,12 +4,25 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/collectors"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
 	fmt.Println("starting http server...")
+
+	// disable default process and go metrics collectors, too noisy
+	prometheus.Unregister(collectors.NewGoCollector())
+	prometheus.Unregister(collectors.NewProcessCollector(collectors.ProcessCollectorOpts{
+		PidFn: func() (int, error) {
+			return os.Getpid(), nil
+		},
+		Namespace: "",
+	}))
+
 	http.HandleFunc("/health", health)
 	http.Handle("/metrics", promhttp.Handler())
 	http.ListenAndServe(":8080", nil)
